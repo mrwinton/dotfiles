120d5294ea74270d45d04e9cbfa3dfad308502fa
Michael Winton <wintonmr@gmail.com>
2018-01-09 19:11:36 +1300

    Update 'pure' links on terminal start up # 50-character subject line # # 72-character wrapped longer description. This should answer: # # * Why was this change necessary? # * How does it address the problem? # * Are there any side effects? # # Include a link to the ticket, if any.

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -1,6 +1,6 @@
 # configure links
-ln -s ~/.zshrc.d/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
-ln -s ~/.zshrc.d/pure/async.zsh /usr/local/share/zsh/site-functions/async
+ln -nsf ~/.zshrc.d/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
+ln -nsf ~/.zshrc.d/pure/async.zsh /usr/local/share/zsh/site-functions/async
 
 # initialize the prompt system (if not so already) and choose pure
 autoload -U promptinit; promptinit
