9565f552514cd3c49877a5e4ab21113abc9d5b7e
Michael Winton <wintonmr@gmail.com>
2018-01-09 19:15:35 +1300

    Quieten standard out messages while linking 'pure' # 50-character subject line # # 72-character wrapped longer description. This should answer: # # * Why was this change necessary? # * How does it address the problem? # * Are there any side effects? # # Include a link to the ticket, if any.

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -1,6 +1,6 @@
 # configure links
-ln -nsf ~/.zshrc.d/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
-ln -nsf ~/.zshrc.d/pure/async.zsh /usr/local/share/zsh/site-functions/async
+ln -nsf ~/.zshrc.d/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup >/dev/null
+ln -nsf ~/.zshrc.d/pure/async.zsh /usr/local/share/zsh/site-functions/async >/dev/null
 
 # initialize the prompt system (if not so already) and choose pure
 autoload -U promptinit; promptinit
