ae9747e5a532d30a6e35876346a337bbd349ae02
Michael Winton <wintonmr@gmail.com>
2018-01-12 20:08:44 +1300

    Add better completions for "zsh"

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -5,3 +5,20 @@ ln -nsf ~/.zshrc.d/pure/async.zsh /usr/local/share/zsh/site-functions/async >/de
 # initialize the prompt system (if not so already) and choose pure
 autoload -U promptinit; promptinit
 prompt pure
+
+# better tab completion
+fpath=(~/.zshrc.d/completions/src $fpath)
+
+# better history searching with arrow keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
+# save command distory
+HISTFILE=~/.zsh_history
+HISTSIZE=SAVEHIST=10000
+setopt sharehistory
+setopt extendedhistory
