fc85b55726ef88efea783d7064c98ea60c1a7ad7
Michael Winton <wintonmr@gmail.com>
2018-02-21 22:38:26 +1300

    Comment out Flux specific aliases

--- zsh/.zshrc.d/aliases.zsh
+++ zsh/.zshrc.d/aliases.zsh
@@ -27,43 +27,43 @@ alias oports="echo 'User:      Command:   Port:'; echo '------------------------
 alias serve="python -m SimpleHTTPServer"
 
 ### Flux
-alias sourcetree='open -a SourceTree .'
-alias be='bundle exec'
+# alias sourcetree='open -a SourceTree .'
+# alias be='bundle exec'
 
-alias nz='PS_MARKET=nz bundle exec'
-alias nzs='PS_MARKET=nz bundle exec spring'
+# alias nz='PS_MARKET=nz bundle exec'
+# alias nzs='PS_MARKET=nz bundle exec spring'
 
-alias au='PS_MARKET=au bundle exec'
-alias aus='PS_MARKET=au bundle exec spring'
+# alias au='PS_MARKET=au bundle exec'
+# alias aus='PS_MARKET=au bundle exec spring'
 
-alias uk='PS_MARKET=uk bundle exec'
-alias uks='PS_MARKET=uk bundle exec spring'
+# alias uk='PS_MARKET=uk bundle exec'
+# alias uks='PS_MARKET=uk bundle exec spring'
 
-alias compress_nz='nz rake db:compress\[9999\]'
-alias compress_au='au rake db:compress\[9999\]'
-alias compress_uk='uk rake db:compress\[9999\]'
+# alias compress_nz='nz rake db:compress\[9999\]'
+# alias compress_au='au rake db:compress\[9999\]'
+# alias compress_uk='uk rake db:compress\[9999\]'
 
-alias ps_prod='git fetch && git describe --abbrev=0 origin/current-nz-prod-app --tags'
-alias spring_stahp='nz spring stop && au spring stop && uk spring stop'
+# alias ps_prod='git fetch && git describe --abbrev=0 origin/current-nz-prod-app --tags'
+# alias spring_stahp='nz spring stop && au spring stop && uk spring stop'
 
-alias cdps='cd $HOME/Development/powershop/'
-alias cdpsbau='cd $HOME/Development/powershop-banking-au/'
-alias cdpss='cd $HOME/Development/powershop-scripts/'
+# alias cdps='cd $HOME/Development/powershop/'
+# alias cdpsbau='cd $HOME/Development/powershop-banking-au/'
+# alias cdpss='cd $HOME/Development/powershop-scripts/'
 
-alias aussie='au rails s -b 127.0.0.1'
-alias kiwi='nz rails s -b 127.0.0.1'
-alias england='uk rails s -b 127.0.0.1'
-alias ausetup='bundle; spring_stahp; au rake db:migrate; au rake db:test:nuke'
-alias nzsetup='bundle; spring_stahp; nz rake db:migrate; nz rake db:test:nuke'
-alias uksetup='bundle; spring_stahp; uk rake db:migrate; uk rake db:test:nuke'
+# alias aussie='au rails s -b 127.0.0.1'
+# alias kiwi='nz rails s -b 127.0.0.1'
+# alias england='uk rails s -b 127.0.0.1'
+# alias ausetup='bundle; spring_stahp; au rake db:migrate; au rake db:test:nuke'
+# alias nzsetup='bundle; spring_stahp; nz rake db:migrate; nz rake db:test:nuke'
+# alias uksetup='bundle; spring_stahp; uk rake db:migrate; uk rake db:test:nuke'
 
-alias ret='RAILS_ENV=test'
+# alias ret='RAILS_ENV=test'
 
-eval "$(rbenv init -)"
+# eval "$(rbenv init -)"
 
-psactive () {
-  [[ `host secure.powershop.co.nz|cut -d" " -f4` == `host akl.secure.powershop.co.nz|cut -d" " -f4` ]] && echo akl
-  [[ `host secure.powershop.co.nz|cut -d" " -f4` == `host wlg.secure.powershop.co.nz|cut -d" " -f4` ]] && echo wlg
-}
+# psactive () {
+#   [[ `host secure.powershop.co.nz|cut -d" " -f4` == `host akl.secure.powershop.co.nz|cut -d" " -f4` ]] && echo akl
+#   [[ `host secure.powershop.co.nz|cut -d" " -f4` == `host wlg.secure.powershop.co.nz|cut -d" " -f4` ]] && echo wlg
+# }
 
-alias reset_banking='bundle; be rails db:drop; be rails db:create; be rails db:migrate; be rails db:test:prepare'
+# alias reset_banking='bundle; be rails db:drop; be rails db:create; be rails db:migrate; be rails db:test:prepare'
