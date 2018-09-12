4c88dcb89041f8170781a7364b4746be40dad4a3
Michael Winton <wintonmr@gmail.com>
2018-09-12 15:52:30 +0200

    Add `ret`, `reset` and `seed` aliases

--- zsh/.zshrc.d/aliases.zsh
+++ zsh/.zshrc.d/aliases.zsh
@@ -22,6 +22,9 @@ usage() {
 ### Ruby/Rails-specific
 alias be="bundle exec"
 alias migrate="be rake db:migrate db:test:prepare"
+alias reset="be rake db:drop db:create db:migrate db:test:prepare"
+alias seed="be rake db:seed"
+alias ret="RAILS_ENV=test"
 
 ### Networking
 alias oports="echo 'User:      Command:   Port:'; echo '----------------------------' ; lsof -i 4 -P -n | grep -i 'listen' | awk '{print \$3, \$1, \$9}' | sed 's/ [a-z0-9\.\*]*:/ /' | sort -k 3 -n |xargs printf '%-10s %-10s %-10s\n' | uniq"
