92feea40439c647339510af7214e37516f367853
Michael Winton <wintonmr@gmail.com>
2019-03-01 08:45:51 +0100

    Help keep .emacs.d clean

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,4 @@
+;; Produce backtraces when errors occur
 (setq debug-on-error t)
 
 ;; Configure package.el to include MELPA.
@@ -15,15 +16,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (moody use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
