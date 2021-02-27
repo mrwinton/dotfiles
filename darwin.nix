daa4c4b9d712fa2fa857624edd5d94e62c0c9d95
Michael Winton <wintonmr@gmail.com>
2021-02-27 15:57:07 +0100

    Remove unnecessary comments

--- darwin.nix
+++ darwin.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
-{
+
+let
+  homeDir = builtins.getEnv("HOME");
+in {
   users.users.michaelwinton = {
-    home = "/Users/michaelwinton";
+    home = homeDir;
     description = "Michael Winton";
     shell = pkgs.zsh;
   };
@@ -11,18 +14,9 @@
     loginShell = "/run/current-system/sw/bin/zsh";
   };
 
-  # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/src/dotfiles/darwin.nix
-  environment.darwinConfig = "$HOME/src/dotfiles/darwin.nix";
+  environment.darwinConfig = "${homeDir}/src/dotfiles/darwin.nix";
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  # environment.systemPackages = with pkgs; [
-  #   coreutils
-  #   openssl
-  # ];
-
-  # List fonts installed in system profile
   fonts = {
     enableFontDir = true;
     fonts = with pkgs; [
@@ -31,11 +25,6 @@
     ];
   };
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
-
-  # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -48,34 +37,25 @@
 
   system.defaults = {
     finder = {
-      # Show me the file extensions
-      AppleShowAllExtensions = true;
-      # Don't pop-up when changing extension
       FXEnableExtensionChangeWarning = false;
     };
     screencapture = {
-      # Save screenshots to the downloads
-      location = "$HOME/Downloads";
+      location = "${homeDir}/Downloads";
     };
     trackpad = {
-      # Enable tap-to-click
       Clicking = true;
-      # Enable three-finger-drag-to-select
       TrackpadThreeFingerDrag = true;
-    };
-    NSGlobalDomain = {
-      # Show me the file extensions
-      AppleShowAllExtensions = true;
-      # Show the bars when scrolling
-      AppleShowScrollBars = "Automatic";
     };
   };
 
   system.activationScripts.postActivation.text = ''
-    # Use list view in all Finder windows by default
+    # Use list view in Finder by default
     defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
   '';
 
-  # Avoid backwards incompatible changes.
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
   system.stateVersion = 4;
 }
