d1a2ebbf9f9e5a1368ded35a8f7e7b9aa9c074d5
Michael Winton <wintonmr@gmail.com>
2021-03-20 19:15:47 +0100

    Add default-gems
    https://github.com/asdf-vm/asdf-ruby#default-gems

--- asdf/config.nix
+++ asdf/config.nix
@@ -4,4 +4,15 @@
   home.file = {
     ".asdfrc".text = "legacy_version_file = yes";
   };
+
+  home.file = {
+    ".default-gems".text = ''
+      awesome_print
+      pry
+      rubocop
+      rubocop-performance
+      rubocop-rails
+      solargraph
+    '';
+  };
 }
