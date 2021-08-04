4d5b0d0681338e8226ef80aafc7b35c41b7bf11c
Michael Winton <wintonmr@gmail.com>
2021-08-05 00:01:25 +0200

    Make smartparens and web-mode play nice

--- emacs/emacs.d/modules/languages/init-web.el
+++ emacs/emacs.d/modules/languages/init-web.el
@@ -3,12 +3,32 @@
 ;;; Code:
 
 (use-package web-mode
-  :mode ("\\.html\\'" "\\.html.erb\\'")
-  :defines company-backends
+  :after smartparens
+  :mode
+  (("\\.html?\\'" . web-mode)
+   ("\\.phtml?\\'" . web-mode)
+   ("\\.tpl\\.php\\'" . web-mode)
+   ("\\.[agj]sp\\'" . web-mode)
+   ("\\.as[cp]x\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)
+   ("\\.djhtml\\'" . web-mode))
   :config
-  ;; Indent by 2 spaces by default
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2
+	      web-mode-css-indent-offset 2
+	      web-mode-code-indent-offset 2
+	      web-mode-enable-auto-quoting t
+	      web-mode-enable-auto-pairing nil)
+
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "%" "%"
+                   :unless '(sp-in-string-p)
+                   :post-handlers '(((lambda (&rest _ignored)
+                                       (just-one-space)
+                                       (save-excursion (insert " ")))
+                                     "SPC" "=" "#")))
+    (sp-local-tag "%" "<% "  " %>")
+    (sp-local-tag "=" "<%= " " %>")
+    (sp-local-tag "#" "<%# " " %>")))
 
 (provide 'init-web)
