a6f0a8ddf162da1693b57523a6665b54e61fe552
Michael Winton <wintonmr@gmail.com>
2021-08-08 23:16:06 +0200

    Update flycheck

--- emacs/emacs.d/modules/editor/init-flycheck.el
+++ emacs/emacs.d/modules/editor/init-flycheck.el
@@ -4,9 +4,17 @@
 
 (use-package flycheck
   :defer 1
+  :commands (flycheck-list-errors flycheck-buffer flycheck-add-next-checker)
   :config
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-check-syntax-automatically '(save idle-change new-line mode-enabled))
   (global-flycheck-mode))
+
+(use-package flycheck-posframe
+  :after flycheck
+  :hook (flycheck-mode . flycheck-posframe-mode)
+  :config
+  (flycheck-posframe-configure-pretty-defaults))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
