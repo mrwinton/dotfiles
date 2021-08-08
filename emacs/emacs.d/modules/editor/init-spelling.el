57803169d34cc634626eeea3c19958c18cbefcbb
Michael Winton <wintonmr@gmail.com>
2021-08-08 23:16:20 +0200

    Add flyspell

--- emacs/emacs.d/modules/editor/init-spelling.el
+++ emacs/emacs.d/modules/editor/init-spelling.el
@@ -14,5 +14,28 @@
             ispell-really-hunspell t
             ispell-extra-args '("-a" "-i" "utf-8")))))
 
+(use-package flyspell
+  :defer t
+  :diminish flyspell-mode
+  :hook ((text-mode . flyspell-mode)
+         (prog-mode . flyspell-prog-mode)))
+
+(use-package flyspell-lazy
+  :after flyspell
+  :commands (flyspell-lazy-mode)
+  :defines (flyspell-lazy-idle-seconds
+            flyspell-lazy-window-idle-seconds)
+  :config
+  (setq flyspell-lazy-idle-seconds 1
+        flyspell-lazy-window-idle-seconds 3)
+  (flyspell-lazy-mode +1))
+
+(use-package flyspell-correct
+  :after flyspell
+  :bind (:map flyspell-mode-map ("C-;" . flyspell-correct-wrapper)))
+
+(use-package flyspell-correct-popup
+  :after flyspell-correct)
+
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
