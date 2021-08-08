439fa155b0f5d00086e3e57b80b1d63d1549da6c
Michael Winton <wintonmr@gmail.com>
2021-08-08 23:15:42 +0200

    Update completion to use hotfuzz

--- emacs/emacs.d/modules/editor/init-completion.el
+++ emacs/emacs.d/modules/editor/init-completion.el
@@ -2,58 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Package `selectrum' is an incremental completion and narrowing
-;; framework. Like Ivy and Helm, which it improves on, Selectrum
-;; provides a user interface for choosing from a list of options by
-;; typing a query to narrow the list, and then selecting one of the
-;; remaining candidates. This offers a significant improvement over
-;; the default Emacs interface for candidate selection.
-(use-package selectrum
-  :straight (:host github :repo "raxod502/selectrum")
-  :defer t
-  :init
-
-  ;; This doesn't actually load Selectrum.
-  (selectrum-mode +1))
-
-;; Package `prescient' is a library for intelligent sorting and
-;; filtering in various contexts.
-(use-package prescient
-  :config
-
-  ;; Remember usage statistics across Emacs sessions.
-  (prescient-persist-mode +1)
-
-  ;; The default settings seem a little forgetful to me. Let's try
-  ;; this out.
-  (setq prescient-history-length 1000))
-
-;; Package `selectrum-prescient' provides intelligent sorting and
-;; filtering for candidates in Selectrum menus.
-(use-package selectrum-prescient
-  :straight (:host github :repo "raxod502/prescient.el"
-                   :files ("selectrum-prescient.el"))
-  :defer 1
-  :after selectrum
-  :config
-
-  (selectrum-prescient-mode +1))
-
-;; Enable richer annotations using the Marginalia package
-(use-package marginalia
-  :after (selectrum)
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
-  :bind (("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-
-  ;; The :init configuration is always executed (Not lazy!)
-  :init
-
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
-  (marginalia-mode))
-
 (use-package consult
   :after selectrum
   :preface
@@ -87,41 +35,58 @@
   :init (advice-add #'completing-read-multiple
                     :override #'consult-completing-read-multiple)
   :config
-  (setq completion-styles '(substring))
   (autoload 'projectile-project-root "projectile")
   (setq consult-project-root-function #'projectile-project-root))
 
+(use-package hotfuzz
+  :defer 1
+  :straight (hotfuzz :type git :host github :repo "axelf4/hotfuzz")
+  :config
+  (hotfuzz-selectrum-mode +1))
+
+(use-package selectrum
+  :config
+  (selectrum-mode +1)
+  :bind (:map selectrum-minibuffer-map
+              ("C-j" . selectrum-next-candidate)
+              ("C-k" . selectrum-previous-candidate)))
+
+(use-package selectrum-prescient
+  :defer 1
+  :config
+  ;; to make sorting and filtering more intelligent
+  (selectrum-prescient-mode +1)
+
+  ;; to save your command history on disk, so the sorting gets more
+  ;; intelligent over time
+  (prescient-persist-mode +1))
+
+(use-package marginalia
+  :after selectrum
+  :config
+  (add-to-list 'marginalia-prompt-categories '("Find file" . file))
+  (marginalia-mode))
+
 (use-package embark
   :after selectrum
-  :bind (:map minibuffer-local-map
-        ("C-c C-o" . embark-export)
-        ("C-c C-a" . embark-act)
-        ("C-c C-c" . embark-collect-snapshot))
+  :bind (("C-." . embark-act)
+         :map minibuffer-local-map
+         ("C-c C-o" . embark-export)
+         ("C-c C-a" . embark-act)
+         ("C-c C-c" . embark-collect-snapshot))
   :commands (embark-dwim embark-act embark-prefix-help-command)
-  :init
-  (setq prefix-help-command #'embark-prefix-help-command)
   :config
-  ;; https://github.com/oantolin/embark/wiki/Additional-Configuration
+  (setq prefix-help-command #'embark-prefix-help-command)
   (setq embark-action-indicator
-	      (lambda (map &optional _target)
-	        (which-key--show-keymap "Embark" map nil nil 'no-paging)
-	        #'which-key--hide-popup-ignore-command)
-	      embark-become-indicator embark-action-indicator)
-
-  (defun refresh-selectrum ()
-    (setq selectrum--previous-input-string nil))
-
-  (add-hook 'embark-pre-action-hook #'refresh-selectrum) 
-
-  (defun shrink-selectrum ()
-    (when (eq embark-collect--kind :live)
-      (with-selected-window (active-completion-window)
-	      (setq-local selectrum-num-candidates-displayed 1)
-	      (setq-local selectrum-display-style
-		                '(horizontal :before-candidates "[" :after-candidates "]"
-				                         :more-candidates "" :candidates-separator "")))))
-
-  (add-hook 'embark-collect-mode-hook #'shrink-selectrum))
+        (lambda (map _target)
+          (which-key--show-keymap "Embark" map nil nil 'no-paging)
+          #'which-key--hide-popup-ignore-command)
+        embark-become-indicator embark-action-indicator)
+  (add-hook 'embark-collect-post-revert-hook
+            (defun resize-embark-collect-window (&rest _)
+              (when (memq embark-collect--kind '(:live :completions))
+                (fit-window-to-buffer (get-buffer-window)
+                                      (floor (frame-height) 2) 1)))))
 
 (use-package embark-consult
   :after
