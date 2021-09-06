bd7d0f3ed358963fa604d46fb658a36dfbc06cf8
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:33:29 +0200

    Update init-lua.el

--- emacs/emacs.d/modules/languages/init-lua.el
+++ emacs/emacs.d/modules/languages/init-lua.el
@@ -3,8 +3,9 @@
 ;;; Code:
 
 (use-package lua-mode
-  :init
-  ;; lua-indent-level defaults to 3 otherwise. Madness.
-  (setq lua-indent-level 2))
+  :mode (("\\.lua\\'" . lua-mode))
+  :custom
+  (lua-indent-level 2))
 
 (provide 'init-lua)
+;;; init-lua.el ends here
