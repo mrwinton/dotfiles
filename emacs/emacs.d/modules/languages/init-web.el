cecc1da87e830d154f23296eb64d4bcb8ed30178
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:34:39 +0200

    Update init-web.el

--- emacs/emacs.d/modules/languages/init-web.el
+++ emacs/emacs.d/modules/languages/init-web.el
@@ -13,13 +13,20 @@
    ("\\.erb\\'" . web-mode)
    ("\\.mustache\\'" . web-mode)
    ("\\.djhtml\\'" . web-mode))
+  :custom
+  (web-mode-markup-indent-offset 2)
+	(web-mode-css-indent-offset 2)
+	(web-mode-code-indent-offset 2)
+  (web-mode-script-padding 2)
+  (web-mode-style-padding 2)
+	(web-mode-enable-auto-quoting t)
+	(web-mode-enable-auto-pairing nil)
+  (web-mode-enable-auto-closing t) ;; Autocomplete </ instantly.
+  (web-mode-auto-close-style 2) ;; Insert matching tags automatically.
+  ;; Disable `web-mode' automatically re-indenting a bunch of
+  ;; surrounding code when you paste anything.
+  (web-mode-enable-auto-indentation nil)
   :config
-  (setq web-mode-markup-indent-offset 2
-	      web-mode-css-indent-offset 2
-	      web-mode-code-indent-offset 2
-	      web-mode-enable-auto-quoting t
-	      web-mode-enable-auto-pairing nil)
-
   (sp-with-modes '(web-mode)
     (sp-local-pair "%" "%"
                    :unless '(sp-in-string-p)
@@ -32,3 +39,4 @@
     (sp-local-tag "#" "<%# " " %>")))
 
 (provide 'init-web)
+;;; init-web.el ends here
