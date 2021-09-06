b9f16b92f379eeda51f99edc60db16760f032bf0
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:24:47 +0200

    Update init-completion.el

--- emacs/emacs.d/modules/editor/init-completion.el
+++ emacs/emacs.d/modules/editor/init-completion.el
@@ -2,6 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package selectrum
+  :hook (after-init . selectrum-mode)
+  :bind (:map selectrum-minibuffer-map
+              ("C-j" . selectrum-next-candidate)
+              ("C-k" . selectrum-previous-candidate))
+  :config
+  (selectrum-mode +1))
+
 (use-package consult
   :after selectrum
   :preface
@@ -24,7 +32,8 @@
   ("C-?"   . mrwinton/consult-line-symbol-at-point)
   ("M-?"   . mrwinton/consult-grep-at-point)
   ("M-y"   . consult-yank-pop)
-  ("M-g g" . consult-line)
+  ("M-g g" . consult-goto-line)
+  ([remap goto-line] . consult-goto-line)
   :custom
   (consult-async-input-debounce 0.1)
   (consult-async-input-throttle 0.2)
@@ -32,27 +41,22 @@
   (consult-find-command "fd -HLp -E .git -c never -t f ARG OPTS")
   (consult-line-numbers-widen t)
   (consult-narrow-key "<")
-  :init (advice-add #'completing-read-multiple
-                    :override #'consult-completing-read-multiple)
+  (consult-project-root-function #'projectile-project-root)
+  :init
+  (advice-add #'completing-read-multiple
+              :override #'consult-completing-read-multiple)
   :config
-  (autoload 'projectile-project-root "projectile")
-  (setq consult-project-root-function #'projectile-project-root))
+  (autoload 'projectile-project-root "projectile"))
 
 (use-package hotfuzz
-  :defer 1
   :straight (hotfuzz :type git :host github :repo "axelf4/hotfuzz")
+  :defer 1
+  :after selectrum
   :config
   (hotfuzz-selectrum-mode +1))
 
-(use-package selectrum
-  :config
-  (selectrum-mode +1)
-  :bind (:map selectrum-minibuffer-map
-              ("C-j" . selectrum-next-candidate)
-              ("C-k" . selectrum-previous-candidate)))
-
 (use-package selectrum-prescient
-  :defer 1
+  :after selectrum
   :config
   ;; to make sorting and filtering more intelligent
   (selectrum-prescient-mode +1)
@@ -63,6 +67,7 @@
 
 (use-package marginalia
   :after selectrum
+  :defer 1
   :config
   (add-to-list 'marginalia-prompt-categories '("Find file" . file))
   (marginalia-mode))
@@ -75,13 +80,14 @@
          ("C-c C-a" . embark-act)
          ("C-c C-c" . embark-collect-snapshot))
   :commands (embark-dwim embark-act embark-prefix-help-command)
+  :custom
+  (prefix-help-command #'embark-prefix-help-command)
+  (embark-action-indicator
+   (lambda (map _target)
+     (which-key--show-keymap "Embark" map nil nil 'no-paging)
+     #'which-key--hide-popup-ignore-command)
+   embark-become-indicator embark-action-indicator)
   :config
-  (setq prefix-help-command #'embark-prefix-help-command)
-  (setq embark-action-indicator
-        (lambda (map _target)
-          (which-key--show-keymap "Embark" map nil nil 'no-paging)
-          #'which-key--hide-popup-ignore-command)
-        embark-become-indicator embark-action-indicator)
   (add-hook 'embark-collect-post-revert-hook
             (defun resize-embark-collect-window (&rest _)
               (when (memq embark-collect--kind '(:live :completions))
@@ -94,25 +100,21 @@
   :hook
   (embark-collect-mode . embark-consult-preview-minor-mode))
 
+(use-package consult-flycheck
+  :ensure t
+  :after (consult flycheck)
+  :bind (:map flycheck-command-map
+              ("!" . consult-flycheck)))
+
 ;; Writable grep buffer
 (use-package wgrep
-  :defer 1
+  :commands wgrep-change-to-wgrep-mode
+  :bind (:map grep-mode-map
+              ("C-c C-q" . wgrep-change-to-wgrep-mode)
+              ("w" . wgrep-change-to-wgrep-mode))
   :custom
   (wgrep-auto-save-buffer t)
   (wgrep-change-readonly-file t))
 
-;;; Finding files
-
-;; Follow symlinks when opening files. This has the concrete impact,
-;; for instance, that when you edit init.el with M-P e e i and then
-;; later do C-x C-f, you will be in the Radian repository instead of
-;; your home directory.
-(setq find-file-visit-truename t)
-
-;; Disable the warning "X and Y are the same file" which normally
-;; appears when you visit a symlinked file by the same name. (Doing
-;; this isn't dangerous, as it will just redirect you to the existing
-;; buffer.)
-(setq find-file-suppress-same-file-warnings t)
-
 (provide 'init-completion)
+;;; init-completion.el ends here
