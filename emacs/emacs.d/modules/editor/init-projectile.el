84e722d917876f71dcaadafd90c184042ddf6531
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:26:15 +0200

    Update init-projectile.el

--- emacs/emacs.d/modules/editor/init-projectile.el
+++ emacs/emacs.d/modules/editor/init-projectile.el
@@ -3,23 +3,23 @@
 ;;; Code:
 
 (use-package projectile
-  :bind-keymap* (("C-c p" . projectile-command-map))
+  :commands (projectile-mode projectile-switch-project)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :custom
+  (projectile-project-search-path '(("~/src" . 2)))
+  (projectile-indexing-method 'alien)
+
+  ;; Use Selectrum (via `completing-read') for Projectile instead of IDO.
+  (projectile-completion-system 'default)
+
+  ;; When switching projects, give the option to choose what to do. This is a
+  ;; way better interface than having to remember ahead of time to use a prefix
+  ;; argument on `projectile-switch-project' (because, and please be honest
+  ;; here, when was the last time you actually remembered to do that?).
+  (projectile-switch-project-action #'projectile-dired)
   :config
-  (setq projectile-project-search-path '(("~/src" . 2)))
-
-  (setq projectile-indexing-method 'alien)
-
-  ;; Use Selectrum (via `completing-read') for Projectile instead of
-  ;; IDO.
-  (setq projectile-completion-system 'default)
-
-  ;; When switching projects, give the option to choose what to do.
-  ;; This is a way better interface than having to remember ahead of
-  ;; time to use a prefix argument on `projectile-switch-project'
-  ;; (because, and please be honest here, when was the last time you
-  ;; actually remembered to do that?).
-  (setq projectile-switch-project-action #'projectile-dired)
-
   (projectile-mode +1))
 
 (provide 'init-projectile)
+;;; init-projectile.el ends here
