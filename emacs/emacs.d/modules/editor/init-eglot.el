1995f38136fa7fa36083e99a0c43cab111f8831c
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:25:26 +0200

    Update init-eglot.el

--- emacs/emacs.d/modules/editor/init-eglot.el
+++ emacs/emacs.d/modules/editor/init-eglot.el
@@ -5,11 +5,12 @@
 (use-package eglot
   :hook (((ruby-mode enh-ruby-mode) . eglot-ensure)
          (js2-mode . eglot-ensure))
+  :custom
+  (eglot-autoshutdown t)
+  (eglot-sync-connect nil)
+  (eglot-autoreconnect nil)
   :config
-  (add-to-list 'eglot-server-programs '(enh-ruby-mode "solargraph" "socket" "--port" :autoport))
-  (setq eglot-autoshutdown t
-        eglot-sync-connect nil
-        eglot-autoreconnect nil))
+  (add-to-list 'eglot-server-programs '(enh-ruby-mode "solargraph" "socket" "--port" :autoport)))
 
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
