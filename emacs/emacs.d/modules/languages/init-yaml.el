a3c580c5b8da6e17d5c8ce5d3bf992c5fe9bd478
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:34:50 +0200

    Update init-yaml.el

--- emacs/emacs.d/modules/languages/init-yaml.el
+++ emacs/emacs.d/modules/languages/init-yaml.el
@@ -4,6 +4,9 @@
 
 (use-package yaml-mode
   :mode ("\\.yml\\'" "\\.yaml\\'" "\\.yaml.erb\\'"))
-(use-package yaml-tomato)
+
+(use-package yaml-tomato
+  :mode ("\\.yml\\'" "\\.yaml\\'" "\\.yaml.erb\\'"))
 
 (provide 'init-yaml)
+;;; init-yaml.el ends here
