30b8b8715c5483a88ffc13af29fab81014d79cfa
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:24:13 +0200

    Update init-advice.el

--- emacs/emacs.d/modules/editor/init-advice.el
+++ emacs/emacs.d/modules/editor/init-advice.el
@@ -2,14 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
+;; Package `which-key' displays the key bindings and associated
+;; commands following the currently-entered key prefix in a popup.
 (use-package which-key
-  :init (which-key-mode)
-  :config (setq which-key-idle-delay 1))
+  :defer 1
+  :custom
+  (which-key-idle-delay 1)
+  :config
+  (which-key-mode))
 
 (use-package helpful
+  :commands (helpful-callable helpful-key helpful-variable helpful-command helpful-at-point)
   :bind (("C-h f"   . helpful-callable)
          ("C-h k"   . helpful-key)
          ("C-h v"   . helpful-variable)
+         ("C-h x"   . helpful-command)
          ("C-h C-h" . helpful-at-point)))
 
 (provide 'init-advice)
