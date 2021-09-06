29be3314afa25e785d128de5b55573f4b52cec82
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:25:54 +0200

    Update init-formatter.el

--- emacs/emacs.d/modules/editor/init-formatter.el
+++ emacs/emacs.d/modules/editor/init-formatter.el
@@ -2,15 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Package `apheleia' implements a sophisticated algorithm for
-;; applying code formatters asynchronously on save without moving
-;; point or modifying the scroll position.
-;;(use-package apheleia
-;;  :init
-;;  (apheleia-global-mode +1))
-
 (use-package format-all
-  :after (exec-path-from-shell))
+  :commands (format-all-buffer format-all-mode)
+  :hook (prog-mode . format-all-mode))
 
 (provide 'init-formatter)
 ;;; init-formatter.el ends here
