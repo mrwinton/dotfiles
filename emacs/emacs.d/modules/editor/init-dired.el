c56c6afcf515c0a75c2036fc5a8dc2959408bf4d
Michael Winton <wintonmr@gmail.com>
2021-09-06 08:25:10 +0200

    Update init-dired.el

--- emacs/emacs.d/modules/editor/init-dired.el
+++ emacs/emacs.d/modules/editor/init-dired.el
@@ -2,19 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq dired-use-ls-dired nil)
-(setq dired-recursive-deletes 'always)
-(setq dired-recursive-copies 'always)
-(setq dired-clean-up-buffers-too t)
-(setq-default dired-dwim-target t)
-
-;; Human-readable units (KB, MB, etc) in dired buffers, directories first
-(setq-default dired-listing-switches "-alh")
+(use-package dired
+  :straight (:type built-in)
+  :after exec-path-from-shell
+  :custom
+  (dired-listing-switches "-ahl --group-directories-first")
+  (dired-dwim-target t)
+  (dired-use-ls-dired t)
+  (dired-recursive-deletes 'always)
+  (dired-recursive-copies 'always)
+  (dired-clean-up-buffers-too t)
+  (insert-directory-program (executable-find "gls")))
 
 (use-package diff-hl
-  :defer 1
-  :config
-  (add-hook 'dired-mode-hook 'diff-hl-dired-mode))
+  :hook (dired-mode . diff-hl-dired-mode))
 
 (provide 'init-dired)
 ;;; init-dired.el ends here
