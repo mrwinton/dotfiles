31ce855716cb188c7717afb5436dab7c33787d7c
Michael Winton <wintonmr@gmail.com>
2021-09-07 10:35:59 +0200

    Add process manager

---
+++ emacs/emacs.d/modules/editor/init-prodigy.el
@@ -0,0 +1,40 @@
+;;; init-prodigy.el --- Prodigy customisations -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package prodigy
+  :commands (prodigy)
+  :bind (("C-c o" . prodigy))
+  :config
+  (prodigy-define-service
+    :name "smart portal - rails"
+    :cwd "~/src/github.com/UniversalAvenue/smart-portal"
+    :path "~/.nix-profile/bin"
+    :command "nix-shell"
+    :args '("--pure" "--run" "bundle exec rails server")
+    :ready-message ".*Worker.*booted.*"
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "smart portal - webpacker"
+    :cwd "~/src/github.com/UniversalAvenue/smart-portal"
+    :path "~/.nix-profile/bin"
+    :command "nix-shell"
+    :args '("--pure" "--run" "bin/webpack-dev-server")
+    :ready-message ".*Compiled successfully.*"
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop nil)
+
+  (prodigy-define-service
+    :name "papi - rails"
+    :cwd "~/src/github.com/UniversalAvenue/UniversalAvenue"
+    :path "~/.nix-profile/bin"
+    :command "nix-shell"
+    :args '("--pure" "--run" "bundle exec rails server -p 3001")
+    :ready-message ".*Worker.*booted.*"
+    :stop-signal 'kill
+    :kill-process-buffer-on-stop nil))
+
+(provide 'init-prodigy)
+;;; init-prodigy.el ends here
