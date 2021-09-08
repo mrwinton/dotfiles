277ee23cb64f3088257c0d58668d53156642d290
Michael Winton <wintonmr@gmail.com>
2021-09-08 22:31:48 +0200

    Quit active prodigy processes on close

--- emacs/emacs.d/modules/editor/init-prodigy.el
+++ emacs/emacs.d/modules/editor/init-prodigy.el
@@ -36,5 +36,32 @@
     :stop-signal 'kill
     :kill-process-buffer-on-stop nil))
 
+(with-eval-after-load 'prodigy
+  (defvar mrwinton/prodigy-processes (list)
+    "List of processes to kill when closing.")
+
+  (defun mrwinton/prodigy-kill-processes ()
+    "Kill all processes started by `prodigy'"
+    (interactive)
+    (mapc
+     (lambda (process)
+       (when (process-live-p process)
+         (kill-process process)))
+     mrwinton/prodigy-processes)
+    (prog1
+        mrwinton/prodigy-processes
+      (setq mrwinton/prodigy-processes nil)))
+
+  (defun mrwinton/prodigy-add-to-processes (service &rest _args)
+    "Add SERVICE process to `mrwinton/prodigy-processes'."
+    (setq mrwinton/prodigy-processes (cl-remove-if-not #'process-live-p mrwinton/prodigy-processes))
+    (let ((process (plist-get service :process)))
+      (when (process-live-p process)
+        (add-to-list 'mrwinton/prodigy-processes process))))
+
+  (advice-add 'prodigy-start-service :after #'mrwinton/prodigy-add-to-processes)
+
+  (add-hook 'kill-emacs-hook #'mrwinton/prodigy-kill-processes))
+
 (provide 'init-prodigy)
 ;;; init-prodigy.el ends here
