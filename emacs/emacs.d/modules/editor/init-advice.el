a7e9227275ea4aa42d0ec47a356f1a4ce4222c7e
Michael Winton <wintonmr@gmail.com>
2021-10-06 02:40:13 +0200

    Add devdocs package

--- emacs/emacs.d/modules/editor/init-advice.el
+++ emacs/emacs.d/modules/editor/init-advice.el
@@ -19,5 +19,49 @@
          ("C-h x"   . helpful-command)
          ("C-h C-h" . helpful-at-point)))
 
+(use-package devdocs
+  :bind (:map prog-mode-map
+              ("C-c . d" . mrwinton/devdocs-lookup+))
+  :init
+  (defvar devdocs-major-mode-docs-alist
+    '((ruby-mode . ("ruby~3" "rails~6.1"))
+      (enh-ruby-mode . ("ruby~3" "rails~6.1"))
+      (css-mode . ("css"))
+      (html-mode . ("html"))
+      (js-mode . ("javascript"))
+      (js2-mode . ("javascript"))
+      (emacs-lisp-mode . ("elisp")))
+    "Alist of MAJOR-MODE and list of docset names.")
+
+  (mapc
+   (lambda (e)
+     (add-hook (intern (format "%s-hook" (car e)))
+               (lambda ()
+                 (setq-local devdocs-current-docs (cdr e)))))
+   devdocs-major-mode-docs-alist)
+
+  (defun mrwinton/devdocs-lookup+()
+    "Look up a DevDocs documentation entry.
+Install the doc if it's not installed."
+    (interactive)
+
+    ;; Install the doc if it's not installed
+    (mapc
+     (lambda (str)
+       (let* ((docs (split-string str " "))
+              (doc (if (length= docs 1)
+                       (downcase (car docs))
+                     (concat (downcase (car docs)) "~" (downcase (cdr docs))))))
+         (unless (file-exists-p
+                  (expand-file-name "metadata" (expand-file-name doc devdocs-data-dir)))
+           (message "Installing %s" str)
+           (devdocs-install doc))))
+     (alist-get major-mode devdocs-major-mode-docs-alist))
+
+    ;; Lookup the symbol at point
+    (if-let ((symbol (symbol-at-point)))
+        (devdocs-lookup nil (symbol-name symbol))
+      (message "No symbol to lookup!"))))
+
 (provide 'init-advice)
 ;;; init-advice.el ends here
