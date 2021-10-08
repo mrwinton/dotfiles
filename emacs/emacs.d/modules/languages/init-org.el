09529ae9a07e49a6f4c35b4521529636d3fa96fb
Michael Winton <wintonmr@gmail.com>
2021-10-08 19:11:29 +0200

    Formatting

--- emacs/emacs.d/modules/languages/init-org.el
+++ emacs/emacs.d/modules/languages/init-org.el
@@ -9,14 +9,21 @@
   ("C-c r" . org-capture)
   :custom
   (org-directory "~/src/github.com/mrwinton/org")
-  (org-agenda-files '("~/src/github.com/mrwinton/org"))
-  (org-startup-indented t)         ;; Visually indent sections. This looks better for smaller files.
-  (org-src-tab-acts-natively t)    ;; Tab in source blocks should act like in major mode
-  (org-src-preserve-indentation t) ;; Source blocks should keep indent rules
-  (org-log-into-drawer t)          ;; State changes for todos and also notes should go into a Logbook drawer
-  (org-src-fontify-natively t)     ;; Code highlighting in code blocks
-  (org-log-done 'time)             ;; Add closed date when todo goes to DONE state
-  (org-support-shift-select t))    ;; Allow shift selection with arrows.
+  (org-agenda-files '("~/src/github.com/mrwinton/org/todo.org"))
+  ;; Visually indent sections. This looks better for smaller files.
+  (org-startup-indented t)
+  ;; Tab in source blocks should act like in major mode
+  (org-src-tab-acts-natively t)
+  ;; Source blocks should keep indent rules
+  (org-src-preserve-indentation t)
+  ;; State changes for todos and also notes should go into a Logbook drawer
+  (org-log-into-drawer t)
+  ;; Code highlighting in code blocks
+  (org-src-fontify-natively t)
+  ;; Add closed date when todo goes to DONE state
+  (org-log-done 'time)
+  ;; Allow shift selection with arrows.
+  (org-support-shift-select t)
 
 (provide 'init-org)
 ;;; init-org.el ends here
