6eaa0d14a880caef0b02b95f3ba37d549e58c802
Michael Winton <wintonmr@gmail.com>
2023-12-13 19:45:37 +0100

    Remove enh-ruby-mode

--- emacs/emacs.d/lisp/init-langs.el
+++ emacs/emacs.d/lisp/init-langs.el
@@ -86,39 +86,6 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
 (use-package nixpkgs-fmt
   :hook (nix-mode . nixpkgs-fmt-on-save-mode))
 
-(use-package enh-ruby-mode
-  :mode (("\\.rb\\'"       . enh-ruby-mode)
-         ("\\.ru\\'"       . enh-ruby-mode)
-         ("\\.jbuilder\\'" . enh-ruby-mode)
-         ("\\.gemspec\\'"  . enh-ruby-mode)
-         ("\\.rake\\'"     . enh-ruby-mode)
-         ("Rakefile\\'"    . enh-ruby-mode)
-         ("Gemfile\\'"     . enh-ruby-mode)
-         ("Guardfile\\'"   . enh-ruby-mode)
-         ("Capfile\\'"     . enh-ruby-mode)
-         ("Vagrantfile\\'" . enh-ruby-mode))
-  :custom
-  (enh-ruby-indent-level 2)
-  (enh-ruby-hanging-paren-deep-indent-level 2)
-  (enh-ruby-deep-indent-paren nil)
-  (enh-ruby-add-encoding-comment-on-save nil)
-  (enh-ruby-font-lock-keywords nil)
-  (enh-ruby-font-names nil)
-  (ruby-font-lock-keywords nil)
-  (ruby-insert-encoding-magic-comment nil)
-  (ruby-align-to-stmt-keywords '(def if))
-  :config
-  (setq mrwinton/ensured-gems '("reek" "solargraph"))
-  (defun mrwinton/ensure-gem-installed (gem)
-    "Check if given gem is installed."
-    (unless (executable-find gem)
-      (error "%s is not installed" gem)))
-  (defun mrwinton/ensure-gems-installed ()
-    "Check if mrwinton/ensured-gems are installed."
-    (interactive)
-    (mapcar 'mrwinton/ensure-gem-installed mrwinton/ensured-gems))
-  (add-hook 'enh-ruby-mode-hook #'mrwinton/ensure-gems-installed 5))
-
 (use-package rspec-mode
   :after enh-ruby-mode
   :custom
