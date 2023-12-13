0144f18fafbbd8080f8d93782eb16c46db66db59
Michael Winton <wintonmr@gmail.com>
2023-12-13 18:41:07 +0100

    Fix loading

--- emacs/emacs.d/lisp/init-git.el
+++ emacs/emacs.d/lisp/init-git.el
@@ -64,30 +64,7 @@
   :after magit
   :requires transient
   :bind (:map smerge-mode-map
-         ("C-c m" . mrwinton/smerge-menu))
-  :config
-  (transient-define-prefix mrwinton/smerge-menu
-    "Smerge"
-    [["Navigation"
-      ("p" "prev" smerge-prev)
-      ("n" "next" smerge-next)]
-     ["Keep"
-      ("b" "base"    smerge-keep-base)
-      ("u" "upper"   smerge-keep-upper)
-      ("l" "lower"   smerge-keep-lower)
-      ("a" "all"     smerge-keep-all)
-      ("c" "current" smerge-keep-current)]
-     ["Diff"
-      ("<" "base against upper"  smerge-diff-base-upper)
-      ("=" "upper against lower" smerge-diff-upper-lower)
-      (">" "base against lower"  smerge-diff-base-lower)
-      ("R" "refine"              smerge-refine)
-      ("E" "ediff"               smerge-ediff)]
-     ["Other"
-      ("C" "combine"   smerge-combine-with-next)
-      ("r" "resolve"   smerge-resolve)
-      ("k" "kill"      smerge-kill-current)
-      ("h" "highlight" smerge-refine)]]))
+              ("C-c m" . mrwinton/smerge-menu)))
 
 (provide 'init-git)
 ;;; init-git.el ends here
