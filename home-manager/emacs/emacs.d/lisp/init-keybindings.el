d7554dd0aba9d859763f377bdbeb99736263d1d9
Michael Winton <wintonmr@gmail.com>
2025-08-12 20:55:29 +0200

    Update keybindings

--- home-manager/emacs/emacs.d/lisp/init-keybindings.el
+++ home-manager/emacs/emacs.d/lisp/init-keybindings.el
@@ -4,10 +4,9 @@
 
 (use-package which-key
   :defer 1
-  :init
+  :config
   (which-key-mode)
   (which-key-setup-minibuffer)
-  :config
   (setq which-key-idle-delay 0.3)
   (setq which-key-prefix-prefix "◉ ")
   (setq which-key-sort-order 'which-key-key-order-alpha
@@ -27,10 +26,10 @@
    "/" '(mrw/rg :which-key "ripgrep")
    ";" '(mrw/deft :which-key "deft")
    ":" '(project-find-file :which-key "p-find file")
-   "." '(counsel-find-file :which-key "find file")
-   "," '(counsel-recentf :which-key "recent files")
+   "." '(find-file :which-key "find file")
+   "," '(consult-recent-file :which-key "recent files")
    "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-   "SPC" '(counsel-find-file :which-key "M-x")
+   "SPC" '(execute-extended-command :which-key "M-x")
    "q" '(save-buffers-kill-terminal :which-key "quit emacs")
    "r" '(jump-to-register :which-key "registers")
    "c" 'org-capture
@@ -46,7 +45,7 @@
 
    ;; Buffers
    "b" '(nil :which-key "buffer")
-   "bb" '(counsel-switch-buffer :which-key "switch buffers")
+   "bb" '(consult-buffer :which-key "switch buffers")
    "bd" '(kill-buffer :which-key "delete buffer")
    "bs" '(mrw/switch-to-scratch-buffer :which-key "scratch buffer")
    "bm" '(mrw/kill-other-buffers :which-key "kill other buffers")
@@ -57,10 +56,10 @@
 
    ;; Files
    "f" '(nil :which-key "files")
-   "fb" '(counsel-bookmark :which-key "bookmarks")
-   "ff" '(counsel-find-file :which-key "find file")
+   "fb" '(consult-bookmark :which-key "bookmarks")
+   "ff" '(find-file :which-key "find file")
    "fn" '(mrw/new-empty-buffer :which-key "new file")
-   "fr" '(counsel-recentf :which-key "recent files")
+   "fr" '(consult-recent-file :which-key "recent files")
    "fR" '(rename-file :which-key "rename file")
    "fs" '(save-buffer :which-key "save buffer")
    "fo" '(reveal-in-osx-finder :which-key "reveal in finder")
@@ -68,11 +67,11 @@
 
    ;; Help/emacs
    "h" '(nil :which-key "help/emacs")
-   "hv" '(counsel-describe-variable :which-key "des. variable")
-   "hb" '(counsel-descbinds :which-key "des. bindings")
+   "hv" '(describe-variable :which-key "des. variable")
+   "hb" '(describe-bindings :which-key "des. bindings")
    "hM" '(describe-mode :which-key "des. mode")
-   "hf" '(counsel-describe-function :which-key "des. func")
-   "hF" '(counsel-describe-face :which-key "des. face")
+   "hf" '(describe-function :which-key "des. func")
+   "hF" '(describe-face :which-key "des. face")
    "hk" '(helpful-key :which-key "des. key")
    "hx" '(helpful-command :which-key "des. command")
 
@@ -82,21 +81,23 @@
    "xs" '(yas-insert-snippet :which-key "insert yasnippet")
    "xf" '(flush-lines :which-key "flush-lines")
    "xj" '(crux-top-join-line :which-key "join lines")
-   "xa" '(mc/mark-next-like-this :which-key "mark all like this")
+   "xw" '(whitespace-cleanup :which-key "cleanup whitespace")
+   "xa" '(mc/mark-all-like-this :which-key "mark all like this")
    "xn" '(mc/mark-next-like-this :which-key "mark next like this")
    "xp" '(mc/mark-previous-like-this :which-key "mark previous like this")
 
 
    ;; Toggles
    "t" '(nil :which-key "toggles")
-   "tt" '(toggle-truncate-lines :which-key "truncate lines")
+   "tl" '(toggle-truncate-lines :which-key "truncate lines")
    "tv" '(visual-line-mode :which-key "visual line mode")
    "tn" '(display-line-numbers-mode :which-key "display line numbers")
    "ta" '(mixed-pitch-mode :which-key "variable pitch mode")
-   "ty" '(counsel-load-theme :which-key "load theme")
+   "ty" '(consult-theme :which-key "load theme")
    "tR" '(read-only-mode :which-key "read only mode")
    "tI" '(toggle-input-method :which-key "toggle input method")
    "tr" '(display-fill-column-indicator-mode :which-key "fill column indicator")
+   "tt" '(mrw/toggle-theme :which-key "toggle theme")
 
    ;; Windows
    "w" '(nil :which-key "window")
@@ -104,7 +105,57 @@
    "w2" '(mrw/split-window-below-and-switch :which-key "split below")
    "w3" '(mrw/split-window-right-and-switch :which-key "split right")
    "wz" '(text-scale-adjust :which-key "text zoom")
+
+   ;; Environment
+   "e" '(:keymap envrc-command-map :package envrc :which-key "envrc")
    ))
+
+;; Additional keybindings from text editing packages
+(general-define-key
+ ;; Global keybindings
+ "C-a" 'crux-move-beginning-of-line
+ "C-o" 'crux-smart-open-line-above
+ "M-o" 'crux-smart-open-line
+ "C-x 4 t" 'crux-transpose-windows
+ "C-x K" 'crux-kill-other-buffers
+ "C-k" 'crux-smart-kill-line
+ "C-x u" 'vundo
+ "M-r" 'vr/query-replace
+ "C-=" 'er/expand-region
+ "C-;" 'flyspell-correct-wrapper
+ [remap ispell-word] 'jinx-correct
+ "M-m" 'mrw-crux-map
+ [M-up] 'md/move-lines-up
+ [M-down] 'md/move-lines-down)
+
+;; Crux prefix map keybindings
+(general-define-key
+ :keymaps 'mrw-crux-map
+ "w" '(crux-view-url :which-key "Open a new buffer containing the contents of URL.")
+ "o" '(crux-open-with :which-key "Open visited file in default external program.")
+ "e" '(crux-sudo-edit :which-key "Edit currently visited file as root.")
+ "i" '(crux-insert-date :which-key "Insert a timestamp according to locale's date and time format.")
+ "t" '(crux-transpose-windows :which-key "Transpose the buffers shown in two windows.")
+ "j" '(crux-top-join-line :which-key "Join the current line with the line beneath it.")
+ "u" '(upcase-dwim :which-key "upcase region if a region is active or word at point.")
+ "d" '(downcase-dwim :which-key "downcase region if a region is active or word at point.")
+ "c" '(capitalize-dwim :which-key "capitalize region if a region is active or word at point.")
+ "r" '(crux-recompile-init :which-key "Byte-compile all your dotfiles again.")
+ "k" '(crux-smart-kill-line :which-key "Kill to the end of the line and kill whole line on the next call.")
+ "M-k" '(crux-kill-line-backwards :which-key "Kill line backwards and adjust the indentation.")
+ "a" '(crux-move-beginning-of-line :which-key "Move point back to indentation/beginning (toggle) of line.")
+ "s" '(crux-ispell-word-then-abbrev :which-key "Call `ispell-word', then create an abbrev for it."))
+
+;; Wgrep keybindings
+(general-define-key
+ :keymaps 'grep-mode-map
+ "C-c C-q" 'wgrep-change-to-wgrep-mode
+ "w" 'wgrep-change-to-wgrep-mode)
+
+;; Projectile keymap
+(general-define-key
+ :prefix "C-c p"
+ "" '(:keymap projectile-command-map :package projectile :which-key "projectile"))
 
 (provide 'init-keybindings)
 ;;; init-keybindings.el ends here
