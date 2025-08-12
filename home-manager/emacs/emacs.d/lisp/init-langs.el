dbc2030764442d86b55fe239a24b1df78c6ab2c2
Michael Winton <wintonmr@gmail.com>
2025-08-12 14:44:05 +0200

    Update lang configs

--- home-manager/emacs/emacs.d/lisp/init-langs.el
+++ home-manager/emacs/emacs.d/lisp/init-langs.el
@@ -4,13 +4,36 @@
 
 (use-package eglot
   :hook (((ruby-mode enh-ruby-mode) . eglot-ensure)
-         ((js2-mode typescript-ts-mode) . eglot-ensure))
+         ((js2-mode typescript-ts-mode js-mode) . eglot-ensure))
   :custom
   (eglot-autoshutdown t)
   (eglot-sync-connect nil)
   (eglot-autoreconnect nil)
+  (eglot-send-changes-idle-time 0.5)
   :config
+  ;; Tab completion integration
+  (setopt tab-always-indent 'complete)
+  (setopt completion-styles '(basic initials substring))
+
+  ;; Better ElDoc
+  (setq eldoc-echo-area-use-multiline-p nil)
+  ;; Ruby language server
   (add-to-list 'eglot-server-programs '(enh-ruby-mode "solargraph" "socket" "--port" :autoport))
+
+  ;; JavaScript/TypeScript language servers
+  (add-to-list 'eglot-server-programs
+               '(js-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+               '(js2-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+               '(typescript-ts-mode "typescript-language-server" "--stdio"))
+  (add-to-list 'eglot-server-programs
+               '(tsx-ts-mode "typescript-language-server" "--stdio"))
+
+  ;; Herb language server for ERB templates
+  ;; Install with: npm install -g @herb-tools/language-server
+  (add-to-list 'eglot-server-programs
+               '(web-mode "herb-language-server" "--stdio"))
 
   (defun mrw/eglot-eldoc-message-function (fmt &rest args)
     "Limit `eldoc-mode' strings to one line.
@@ -29,16 +52,72 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
 
   (add-hook 'eglot--managed-mode-hook #'mrw/eglot--managed-mode-hook))
 
+;; Enhanced CSS support
 (use-package css-mode
-  :mode (("\\.css\\'" . css-mode))
-  :custom
-  (css-indent-offset 2))
+  :mode (("\\.css\\'" . css-mode)))
 
 (use-package scss-mode
   :mode (("\\.scss\\'" . scss-mode))
   :custom
   (scss-compile-at-save nil))
 
+(use-package less-mode
+  :mode (("\\.less\\'" . less-mode)))
+
+;; Modern JavaScript/TypeScript support
+(use-package js2-mode
+  :mode (("\\.js\\'" . js2-mode)
+         ("\\.jsx\\'" . js2-mode))
+  :custom
+  (js2-auto-indent-p t)
+  (js2-enter-indents-newline t)
+  (js2-indent-on-enter-key t)
+  (js2-global-externs '("module" "require" "process" "console" "JSON" "Buffer"))
+  (js2-highlight-level 3)
+  (js2-mode-show-parse-errors nil)
+  (js2-mode-show-strict-warnings nil))
+
+(use-package typescript-ts-mode
+  :mode (("\\.ts\\'" . typescript-ts-mode)
+         ("\\.tsx\\'" . tsx-ts-mode)))
+
+;; JSON support with formatting
+(use-package json-mode
+  :mode (("\\.json\\'" . json-mode)
+         ("\\.jsonc\\'" . json-mode))
+)
+
+(use-package json-reformat
+  :after json-mode
+  :commands (json-reformat-region))
+
+;; Web development support
+(use-package web-mode
+  :mode ("\\.html\\'"
+         "\\.phtml\\'"
+         "\\.tpl\\.php\\'"
+         "\\.[agj]sp\\'"
+         "\\.as[cp]x\\'"
+         "\\.erb\\'"
+         "\\.mustache\\'"
+         "\\.djhtml\\'"
+         "\\.vue\\'"
+         "\\.svelte\\'")
+  :custom
+  (web-mode-enable-auto-quoting t)
+  (web-mode-enable-auto-pairing nil)
+  (web-mode-enable-auto-closing t)
+  (web-mode-auto-close-style 2)
+  (web-mode-enable-auto-indentation t)
+  (web-mode-enable-current-element-highlight t)
+  (web-mode-enable-current-column-highlight t))
+
+;; GraphQL support
+(use-package graphql-mode
+  :mode (("\\.gql\\'" . graphql-mode)
+         ("\\.graphql\\'" . graphql-mode)))
+
+;; Shell script support
 (use-package sh-script
   :straight (:type built-in)
   :mode (("\\.zsh\\'" . sh-mode)
@@ -49,36 +128,25 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
          ("\\.bashrc\\'" . sh-mode)
          ("\\.bashenv\\'" . sh-mode)
          ("\\.bashprofile\\'" . sh-mode))
-  :custom
-  (sh-basic-offset 2)
-  (sh-indentation 2))
+)
 
-(use-package graphql-mode
-  :mode (("\\.gql\\'" . graphql-mode)
-         ("\\.graphql'" . graphql-mode)))
-
-(use-package json-reformat
-  :mode "\\.json'")
-
-(use-package json-mode
-  :mode "\\.json'")
-
+;; Lua support
 (use-package lua-mode
-  :mode (("\\.lua\\'" . lua-mode))
-  :custom
-  (lua-indent-level 2))
+  :mode (("\\.lua\\'" . lua-mode)))
 
+;; Markdown support
 (use-package markdown-mode
   :mode ("/README\\(?:\\.md\\)?\\'" . gfm-mode)
   :custom
-  (markdown-enable-math t) ; syntax highlighting for latex fragments
+  (markdown-enable-math t)
   (markdown-enable-wiki-links t)
   (markdown-italic-underscore t)
   (markdown-asymmetric-header t)
   (markdown-fontify-code-blocks-natively t)
-  (markdown-gfm-additional-languages '("sh"))
+  (markdown-gfm-additional-languages '("sh" "javascript" "typescript" "css" "scss"))
   (markdown-make-gfm-checkboxes-buttons t))
 
+;; Nix support
 (use-package nix-mode
   :interpreter ("\\(?:cached-\\)?nix-shell" . +nix-shell-init-mode)
   :mode "\\.nix\\'")
@@ -86,6 +154,7 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
 (use-package nixpkgs-fmt
   :hook (nix-mode . nixpkgs-fmt-on-save-mode))
 
+;; Ruby support
 (use-package rspec-mode
   :after ruby-mode
   :custom
@@ -96,35 +165,14 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
   (rspec-command-options "--color --format documentation"))
 
 (use-package ruby-tools
-  :hook (ruby-mode . ruby-tools-mode))
+  :hook ((ruby-mode ruby-ts-mode) . ruby-tools-mode))
 
-(use-package web-mode
-  :mode ("\\.html\\'"
-         "\\.phtml\\'"
-         "\\.tpl\\.php\\'"
-         "\\.[agj]sp\\'"
-         "\\.as[cp]x\\'"
-         "\\.erb\\'"
-         "\\.mustache\\'"
-         "\\.djhtml\\'")
-  :custom
-  (web-mode-markup-indent-offset 2)
-	(web-mode-css-indent-offset 2)
-	(web-mode-code-indent-offset 2)
-  (web-mode-script-padding 2)
-  (web-mode-style-padding 2)
-	(web-mode-enable-auto-quoting t)
-	(web-mode-enable-auto-pairing nil)
-  (web-mode-enable-auto-closing t) ;; Autocomplete </ instantly.
-  (web-mode-auto-close-style 2) ;; Insert matching tags automatically.
-  ;; Disable `web-mode' automatically re-indenting a bunch of
-  ;; surrounding code when you paste anything.
-  (web-mode-enable-auto-indentation nil))
-
+;; Scala support
 (use-package scala-mode
   :interpreter
   ("scala" . scala-mode))
 
+;; YAML support
 (use-package yaml-mode
   :mode ("\\.yml\\'" "\\.yaml\\'" "\\.yaml.erb\\'"))
 
@@ -132,14 +180,22 @@ Eglot doesn't heed to `eldoc-echo-area-use-multiline-p'."
   :commands
   (yaml-tomato-show-current-path yaml-tomato-copy))
 
-(use-package typescript-ts-mode
-  :mode (("\\.ts\\'" . typescript-ts-mode)
-         ("\\.tsx\\'" . tsx-ts-mode)))
-
+;; Tree-sitter support for better syntax highlighting
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
   :config
+  ;; Set up language sources
+  (setq treesit-language-source-alist
+        '((typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (yaml "https://github.com/tree-sitter/tree-sitter-yaml")
+          (ruby "https://github.com/tree-sitter/tree-sitter-ruby")))
+
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
