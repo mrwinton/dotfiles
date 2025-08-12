eee9a1a5cdc05235840d2a8ec084e1268241e269
Michael Winton <wintonmr@gmail.com>
2025-08-12 20:56:12 +0200

    Update completion

--- home-manager/emacs/emacs.d/lisp/init-completion.el
+++ home-manager/emacs/emacs.d/lisp/init-completion.el
@@ -6,41 +6,60 @@
   :hook (after-init . global-corfu-mode)
   :custom
   (corfu-auto t)
-  (corfu-auto-delay 0.3)
-  (corfu-auto-prefix 3)
+  (corfu-auto-delay 0.2)
+  (corfu-auto-prefix 2)
   (corfu-quit-at-boundary t)     ;; Automatically quit at word boundary
   (corfu-quit-no-match t)        ;; Automatically quit if there is no match
   (corfu-echo-documentation nil) ;; Do not show documentation in the echo area
-  (corfu-cycle t))
+  (corfu-cycle t)
+  (corfu-preselect 'prompt)      ;; Better default selection
+  :config
+  (corfu-popupinfo-mode 1)       ;; Show documentation popup
+  (corfu-history-mode 1))        ;; Enable completion history
 
 (use-package cape
   :after corfu
   :init
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev))
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  :config
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda () (add-to-list 'completion-at-point-functions #'cape-elisp-symbol t)))
+  (add-hook 'org-mode-hook
+            (lambda () (add-to-list 'completion-at-point-functions #'cape-tex t))))
 
 (use-package dabbrev
   ;; Swap M-/ and C-M-/
   :bind (("M-/" . dabbrev-completion)
          ("C-M-/" . dabbrev-expand)))
 
+;; Add orderless for fuzzy completion matching
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
 (use-package emacs
   :straight (:type built-in)
-  :init
+  :custom
   ;; TAB cycle if there are only few candidates
-  ;; (setq completion-cycle-threshold 3)
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete))
+  (completion-cycle-threshold 3)
+  ;; Hide commands in M-x which do not apply to the current mode
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Enable indentation+completion using the TAB key
+  (tab-always-indent 'complete)
+  ;; Show more details in completions (Emacs 28+)
+  (completions-detailed t)
+  ;; Better completion display
+  (completion-show-help nil)
+  (completion-auto-help 'always))
 
 (use-package yasnippet
-  :hook (((org-mode enh-ruby-mode ruby-mode js2-mode js-mode markdown-mode rspec-mode) . yas-minor-mode)
+  :hook (((org-mode enh-ruby-mode ruby-mode js2-mode js-mode typescript-mode
+           markdown-mode rspec-mode python-mode go-mode rust-mode
+           emacs-lisp-mode lisp-mode clojure-mode) . yas-minor-mode)
          (snippet-mode . (lambda ()
                            ;; Temporarily disable required newline at the end of
                            ;; file This fixes the problem with an extra newline
