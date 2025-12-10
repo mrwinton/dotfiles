dbdcc8076e13cdef0afa12e621879bdf662422a3
Michael Winton <wintonmr@gmail.com>
2025-12-10 08:57:23 +0100

    Make path portable

--- home-manager/emacs/default.nix
+++ home-manager/emacs/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, inputs, ... }:
 
 let
-  dotfilesPath = "~/src/github.com/mrwinton/dotfiles";
+  dotfilesPath = toString ./../..;
   mrwintonPath = "${dotfilesPath}/home-manager/emacs/emacs.d";
   purcellPath = ".config/emacs/purcell.emacs.d";
   doomPath = ".config/emacs/doom.emacs.d";
